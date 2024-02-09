5c1ec89781a46dc784b4c952da80bbf395d4a713
Denbaiii <kkec228@gmail.com>
2024-02-09 19:08:43 +0600

    all

---
+++ account/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import RegistrationView, LoginView, activation_view, ActivationView, UserListView, LogoutView, RegistrationPhoneView, ResetPasswordConfirmView, ResetPasswordConfirmView, ResetPasswordView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+
+urlpatterns = [
+    path('register/', RegistrationView.as_view(), name = 'registration'),
+    path('activate/', ActivationView.as_view()),
+    path('login/', LoginView.as_view(), name = 'login'),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('list_users/', UserListView.as_view()),
+    path('logout/', LogoutView.as_view(), name = 'logout'),
+    path('activation/', activation_view, name = 'activation'),
+    path('register_phone/', RegistrationPhoneView.as_view()),
+    path('reset-password/', ResetPasswordView.as_view()),
+    path('reset-password/confirm/', ResetPasswordConfirmView.as_view()),
+    path('access/', TokenObtainPairView.as_view()),
+    path('token-verify/', TokenVerifyView.as_view()),
+    
+]
---
+++ account/views.py
@@ -0,0 +1,126 @@
+from django.shortcuts import render, redirect
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegistrationSerializer, ActivationSerializer, UserSerializer, RegistrationPhoneSerializer, ResetPasswordSerializer, ConfirmPasswordSerializer
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView, get_object_or_404, ListAPIView
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
+from justlang.tasks import send_confirmation_email_task, send_confirmation_password_task
+
+
+User = get_user_model()
+
+class ActivationView(GenericAPIView):
+    serializer_class = ActivationSerializer
+
+    def get(self, request):
+        code = request.GET.get('u')
+        user = get_object_or_404(User, activation_code = code)
+        user.is_active = True
+        user.save()
+        return Response('Успешно активирован', status=200)
+    
+    def post(self, request):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response('Успешно активирован!', status=200)
+    
+class   LoginView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+
+class UserListView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'message':'Logout successful'}, status=200)
+        except Exception as e:
+            return Response({'error':'Invalid Token'}, status=400)
+                
+
+    class RegistrationView(APIView):
+        def post(self, request):
+            serializer = RegistrationSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+                if user:
+                    try:
+                        send_confirmation_email_task.delay(user.email, user.activation_code)
+                    except:
+                        return Response({"message": "Зарегистрировался, но на почту код не отправился.",
+                                        'data': serializer.data}, status=200)
+                return Response({'message': 'User registered successfully'})
+            else:
+                return Response({'errors': serializer.errors})
+                
+def activation_view(request):
+    return render(request, 'activation.html')
+
+class RegistrationPhoneView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = RegistrationPhoneSerializer(data = data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response('Registered success', status=201)
+    
+class ResetPasswordView(APIView):
+    def get(self, request):
+        return Response({'message': 'Please provide an email to reset the password'})
+    
+    def post(self, request):
+        serializer = ConfirmPasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            try:
+                user = User.objects.get(email = email)
+                user.create_phone_number_code()
+                user.save()
+                # send_confirmation_password(user.email, user.activation_code)
+                send_confirmation_password_task.delay(user.email, user.activation_code)
+                return Response({'activation_code': user.activation_code}, status=200)
+            except:
+                return Response({'message': 'User with this email does not exist!'}, status=404)
+        return Response(serializer.errors, status=400)
+        
+class ResetPasswordConfirmView(APIView):
+    def post(self, request):
+        code = request.GET.get('u')
+        user = get_object_or_404(User, activation_code = code)
+        serializer = ResetPasswordSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        new_password = serializer.validated_data['new_password']
+        user.set_password(new_password)
+        user.activation_code = ''
+        user.save()
+        return Response('Your password has been successfully updated', status=200)
+    
+class ResetPasswordView(APIView):
+    def get(self, request):
+        return Response({'message': 'Please provide an email to reset the password'})
+    
+    def post(self, request):
+        serializer = ConfirmPasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            try:
+                user = User.objects.get(email = email)
+                user.create_phone_number_code()
+                user.save()
+                # send_confirmation_password(user.email, user.activation_code)
+                send_confirmation_password_task.delay(user.email, user.activation_code)
+                return Response({'activation_code': user.activation_code}, status=200)
+            except:
+                return Response({'message': 'User with this email does not exist!'}, status=404)
+        return Response(serializer.errors, status=400)
